2833a666397ac7bf4ba39619a08e8a368ba527ae
Tim <timothy.lawless@gmail.com>
2023-04-22 17:09:57 -0400

    expaned widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,102 @@ import 'package:fileaudioplayer/fileaudioplayer.dart';
 void main() => runApp(XylophoneApp());
 // end main
 
+
+void playSound(int soundNumber){
+
+  final player = FileAudioPlayer();
+  player.start('note$soundNumber.wav');
+
+}
+
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: (){
-                final player = FileAudioPlayer();
-                player.start('note2.wav');
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  onPressed: (){
+                    playSound(1);
+                  } ,
+                  child: Text('Click me'),
+                ),
+              ),
 
-              } ,
-              child: Text('Click me'),
-            ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.pink),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  onPressed: (){
+                    playSound(2);
+                  } ,
+                  child: Text('Click me'),
+                ),
+              ),
+
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.purpleAccent),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  onPressed: (){
+                    playSound(3);
+                  } ,
+                  child: Text('Click me'),
+                ),
+              ),
+
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.redAccent),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  onPressed: (){
+                    playSound(4);
+                  } ,
+                  child: Text('Click me'),
+                ),
+              ),
+
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  onPressed: (){
+                    playSound(5);
+                  } ,
+                  child: Text('Click me'),
+                ),
+              ),
+
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  onPressed: (){
+                    playSound(6);
+                  } ,
+                  child: Text('Click me'),
+                ),
+              ),
+            ],
           ),
         ),
       ),
